57fdf6d3abfc8e1251189c447e880ec16eac2603
Genwaver <genwaver@gmail.com>
2023-07-11 21:20:34 -0600

    ✨ Generate random tiles

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ import * as math from 'mathjs'
 const settings = {
   heightMaxFactor: 0.85,
   heightMinFactor: 0.35,
-
+  buildingCount: 10
 }
 
 const palette = {
   background: '#B9F3E4',
   stroke: '#0000ee',
-  buildingBody: '#EA8FEA'
+  tile1: '#EA8FEA',
+  tile2: '#FFAACF',
+  tile3: '#F6E6C2',
 }
 
 const generateBackground = (view) => {
@@ -22,7 +24,7 @@ const generateBackground = (view) => {
   })
 }
 
-const generateBuilding = (view) => {
+const generateBuilding = (view, x) => {
   // Generate body
   const buildingHeight = math.random(
     settings.heightMinFactor * view.size.height,
@@ -35,14 +37,39 @@ const generateBuilding = (view) => {
   )
 
   const bodySize = new paper.Size(buildingWidth, buildingHeight)
-  const point = view.center.clone().subtract(bodySize.multiply(0.5))
+  const bodyCorner = new paper.Point(x, 0)
+    .add([0, view.size.height])
+    .subtract([0, buildingHeight])
 
   const path = new paper.Path.Rectangle({
-    point: point,
+    point: bodyCorner,
     size: bodySize,
     fillColor: palette.buildingBody,
     strokeColor: palette.stroke
   })
+
+  // Generate tiles
+  const cols = math.randomInt(5, 15)
+  const rows = math.randomInt(10, 25)
+
+  const rowSize = bodySize.height / rows
+  const colSize = bodySize.width / cols
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const xTile = col * colSize
+      const yTile = row * rowSize
+      const tilePoint = new paper.Point(xTile, yTile).add(bodyCorner)
+      
+      const tile = new paper.Path.Rectangle({
+        point: tilePoint,
+        size: [colSize, rowSize],
+        strokeColor: palette.stroke,
+        fillColor: math.pickRandom([palette.tile1, palette.tile2, palette.tile3])
+      })
+    }
+  }
+
 }
 
 window.onload = () => {
@@ -50,7 +77,15 @@ window.onload = () => {
   paper.setup(canvas)
 
   generateBackground(paper.view)
-  generateBuilding(paper.view)
+
+  for (let i = 0; i < settings.buildingCount; i++) {
+    const buildingXSpace = paper.view.size.width / settings.buildingCount
+    const xBuilding = i * buildingXSpace
+    const xOffset = math.random(paper.view.size.width * -0.05, paper.view.size.width * 0.05)
+
+    generateBuilding(paper.view, xBuilding + xOffset)
+  }
+  
 
   paper.view.draw()
   paper.view.onFrame = () => {
